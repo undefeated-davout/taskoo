79300973db1b0196876f71323ec36dfbfdeb771d
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-08-19 15:03:17 +0900

    try: yarn

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,5 +7,5 @@ export const helloWorld = functions
   .region('asia-northeast1')
   .https.onRequest((request, response) => {
     functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase! test');
+    response.send('Hello from Firebase!');
   });
