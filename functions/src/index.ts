dfbb459424df0019dc50d1c12c48817632e01e5a
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-08-19 18:23:16 +0900

    styles: functions フォーマット

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,58 +1,64 @@
-import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
-import { firestore } from 'firebase-admin'
-import { deleteCollection } from './modules/firestore/delete'
+import * as admin from 'firebase-admin';
+import { firestore } from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+import { deleteCollection } from './lib/firestore/delete';
 
 // デフォルト設定済みFunctions
-const defaultFunctions = functions.region('asia-northeast1')
+const defaultFunctions = functions.region('asia-northeast1');
 // Firestore接続情報作成
-const db = admin.initializeApp().firestore()
+const db = admin.initializeApp().firestore();
 
 // 初回認証時処理
 // Userドキュメントを作成する
 export const createUser = defaultFunctions.auth
   .user()
   .onCreate(async (user: admin.auth.UserRecord) => {
-    let result = 'error'
-    let message = 'function failed'
-    // CustomClaimsにロールをセット
-    admin.auth().setCustomUserClaims(user.uid, { })
+    let result = 'error';
+    let message = 'function failed';
+
+    // // CustomClaimsをセット
+    // admin.auth().setCustomUserClaims(user.uid, {});
 
     const mainFunc = async (user: admin.auth.UserRecord) => {
       await db.collection('users').doc(user.uid).set({
         createdAt: firestore.FieldValue.serverTimestamp(),
         updatedAt: firestore.FieldValue.serverTimestamp(),
-      })
-      return true
-    }
-    const isSuccess = await mainFunc(user)
+      });
+      return true;
+    };
+    const isSuccess = await mainFunc(user);
     if (isSuccess) {
-      result = 'success'
-      message = 'user created'
+      result = 'success';
+      message = 'user created';
     }
-    functions.logger.info('result:', result)
-    return { result, message }
-  })
+    functions.logger.info('result:', result);
+    return { result, message };
+  });
 
 // 認証削除時処理
 // 該当ユーザのドキュメント配下全て削除する
 export const deleteUser = defaultFunctions.auth
   .user()
   .onDelete(async (user: admin.auth.UserRecord) => {
-    let result = 'error'
-    let message = 'function failed'
+    let result = 'error';
+    let message = 'function failed';
     const mainFunc = async (user: admin.auth.UserRecord) => {
       // タスク削除
-      await deleteCollection(db,db.collection('users').doc(user.uid).collection('tasks').path,500)
+      await deleteCollection(
+        db,
+        db.collection('users').doc(user.uid).collection('tasks').path,
+        500,
+      );
       // 最後にユーザ削除
-      await db.collection('users').doc(user.uid).delete()
-      return true
-    }
-    const isSuccess = await mainFunc(user)
+      await db.collection('users').doc(user.uid).delete();
+      return true;
+    };
+    const isSuccess = await mainFunc(user);
     if (isSuccess) {
-      result = 'success'
-      message = 'user created'
+      result = 'success';
+      message = 'user created';
     }
-    functions.logger.info('result:', result)
-    return { result, message }
-  })
+    functions.logger.info('result:', result);
+    return { result, message };
+  });
--- functions/src/lib/firestore/delete.ts
+++ functions/src/lib/firestore/delete.ts
@@ -1,4 +1,4 @@
-import { firestore } from 'firebase-admin'
+import { firestore } from 'firebase-admin';
 
 /**
  * コレクション削除処理
@@ -10,14 +10,14 @@ import { firestore } from 'firebase-admin'
 export const deleteCollection = (
   db: firestore.Firestore,
   collectionPath: string,
-  batchSize: number
+  batchSize: number,
 ) => {
-  const collectionRef = db.collection(collectionPath)
-  const query = collectionRef.orderBy('__name__').limit(batchSize)
+  const collectionRef = db.collection(collectionPath);
+  const query = collectionRef.orderBy('__name__').limit(batchSize);
   return new Promise((resolve, reject) => {
-    deleteQueryBatch(db, query, resolve).catch(reject)
-  })
-}
+    deleteQueryBatch(db, query, resolve).catch(reject);
+  });
+};
 
 /**
  * クエリバッチ処理
@@ -29,27 +29,27 @@ export const deleteCollection = (
 async function deleteQueryBatch(
   db: firestore.Firestore,
   query: firestore.Query<firestore.DocumentData>,
-  resolve: any
+  resolve: any,
 ) {
-  const snapshot = await query.get()
+  const snapshot = await query.get();
 
-  const batchSize = snapshot.size
+  const batchSize = snapshot.size;
   if (batchSize === 0) {
     // When there are no documents left, we are done
-    resolve()
-    return
+    resolve();
+    return;
   }
 
   // Delete documents in a batch
-  const batch = db.batch()
+  const batch = db.batch();
   snapshot.docs.forEach((doc) => {
-    batch.delete(doc.ref)
-  })
-  await batch.commit()
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
 
   // Recurse on the next process tick, to avoid
   // exploding the stack.
   process.nextTick(() => {
-    deleteQueryBatch(db, query, resolve)
-  })
+    deleteQueryBatch(db, query, resolve);
+  });
 }
