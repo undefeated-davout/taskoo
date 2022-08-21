23d358e95e3bf33d39328ab0fab72d58cefd42b2
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-08-21 12:30:17 +0900

    add: エミュレータ接続

--- web/src/lib/infrastructure/firebase.ts
+++ web/src/lib/infrastructure/firebase.ts
@@ -1,7 +1,11 @@
-// import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
+import { Auth, connectAuthEmulator, getAuth } from 'firebase/auth';
 import firebase from 'firebase/compat/app';
-import { getFirestore } from 'firebase/firestore';
+import {
+  Firestore,
+  connectFirestoreEmulator,
+  getFirestore,
+} from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -13,8 +17,24 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
+export let auth: Auth;
+export let db: Firestore;
+
 export const preparedFirebase = firebase;
 const app = preparedFirebase.initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === 'development') {
+  auth = getAuth(app);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  db = getFirestore(app);
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  let functions = getFunctions(app);
+  functions.region = 'asia-northeast1';
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+} else {
+  auth = getAuth(app);
+  db = getFirestore(app);
+}
+
+// import { getAnalytics } from 'firebase/analytics';
 // const analytics = getAnalytics(app);
-export const auth = getAuth();
-export const db = getFirestore(app);
