4cc7cf702aa981a84c1971327584c28c30ffae14
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-09-02 16:45:00 +0900

    add: timeNow

---
+++ web/src/lib/util/time.ts
@@ -0,0 +1,20 @@
+export const timeNow = () => {
+  var date = new Date();
+
+  const time =
+    date.getFullYear() +
+    '/' +
+    ('0' + (date.getMonth() + 1)).slice(-2) +
+    '/' +
+    ('0' + date.getDate()).slice(-2) +
+    ' ' +
+    ('0' + date.getHours()).slice(-2) +
+    ':' +
+    ('0' + date.getMinutes()).slice(-2) +
+    ':' +
+    ('0' + date.getSeconds()).slice(-2);
+
+  console.log('time', time);
+
+  return time;
+};
