c0214ff92c486052eb31f0b8628bed0a138363cd
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-09-07 17:33:18 +0900

    refactor: type

--- web/src/types/task.ts
+++ web/src/types/task.ts
@@ -16,3 +16,22 @@ export type kanbanTaskStateType = {
   taskOrderID: string;
   statusIDTasks: statusIDTasksType;
 };
+
+export type kanbanStatusType = {
+  id: string;
+  orderNum: number;
+  name: string;
+};
+
+export const DnDItems = {
+  Task: 'Task',
+} as const;
+
+export type DnDItems = typeof DnDItems[keyof typeof DnDItems];
+
+export type DropResult = {
+  panelID: string;
+  taskID: string;
+  isSetNext: boolean;
+  kanbanTask: kanbanTaskStateType | null;
+};
