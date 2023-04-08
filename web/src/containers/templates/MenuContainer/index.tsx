34d266b0fb56cc87d0b2b25b7e4d1b8e0d47f138
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2023-04-08 19:55:35 +0900

    update: メニュー初期表示時、サイドバーは最小表示にする

--- web/src/containers/templates/MenuContainer/index.tsx
+++ web/src/containers/templates/MenuContainer/index.tsx
@@ -16,7 +16,7 @@ type MenuContainerProps = {
 const MenuContainer = (props: MenuContainerProps) => {
   const theme = useTheme();
   const [cookies, setCookie, _] = useCookies(['isOpenMenu']);
-  const [isMenuOpen, setIsMenuOpen] = useState(cookies.isOpenMenu !== 'false');
+  const [isMenuOpen, setIsMenuOpen] = useState(cookies.isOpenMenu !== 'true');
   const toggleSideBarOpen = () => {
     setCookie('isOpenMenu', !isMenuOpen);
     setIsMenuOpen(!isMenuOpen);
